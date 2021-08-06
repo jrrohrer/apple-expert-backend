30d10ae5dc3aef6f95078cb3669b4ec4e074e7eb
jrrohrer <jessie.rohrer@gmail.com>
2021-08-06 09:59:45 -0400

    debug apples#create action

--- app/controllers/api/v1/apples_controller.rb
+++ app/controllers/api/v1/apples_controller.rb
@@ -3,14 +3,14 @@ class Api::V1::ApplesController < ApplicationController
   def index
     # because I am using a get request to get only apples from the user's requested category, I am sending the category ID back to the controller as a query param. The category variable saves the query param and passes it to an activerecord query method (see bottom of controller) that returns all the Apple instances that match the query. 
     category = params[:category_id].to_i
-    get_apples_by_category(category)
+    @apples = get_apples_by_category(category)
     render json: AppleSerializer.new(@apples)
   end
 
   def create
     byebug
     apple = Apple.create(apple_params)
-    apple.category_ids = params[:category_ids]
+    # apple.category_ids = params[:category_ids]
     if apple.save
       render json: AppleSerializer.new(apple)
     end
@@ -35,7 +35,7 @@ class Api::V1::ApplesController < ApplicationController
   end
 
   def get_apples_by_category(category)
-    @apples = Apple.joins(:apples_categories).where(apples_categories: { category_id: category })
+    Apple.joins(:apples_categories).where(apples_categories: { category_id: category })
   end
 
 end
