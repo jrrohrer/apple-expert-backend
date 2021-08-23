45a222793627edbb0984e47b8d8fb6a0ad6adea4
jrrohrer <jessie.rohrer@gmail.com>
2021-08-23 12:14:26 -0400

    add validations

--- app/models/apple.rb
+++ app/models/apple.rb
@@ -1,5 +1,6 @@
 class Apple < ApplicationRecord
   has_and_belongs_to_many :categories
-
   accepts_nested_attributes_for :categories
+  validates :variety, presence: true
+  validates :categories, presence: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :apples
-
   accepts_nested_attributes_for :apples
 end
